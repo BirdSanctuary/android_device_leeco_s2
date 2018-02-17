49fd95e3d250c2a794dfe807150bea0e4f50715e
Renji <renjian503@gmail.com>
2018-02-17 14:56:06 +0000

    Initial modification for Cardinal-AOSP

--- cardinal.mk
+++ cardinal.mk
@@ -6,11 +6,11 @@ TARGET_SCREEN_HEIGHT := 1920
 $(call inherit-product, device/leeco/s2/full_s2.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cardinal/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := s2
-PRODUCT_NAME := lineage_s2
+PRODUCT_NAME := cardinal_s2
 PRODUCT_BRAND := LeEco
 PRODUCT_MANUFACTURER := LeMobile
 
@@ -24,6 +24,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := Letv/Le2_WW/le_s2_ww:6.0.1/IIXOSOP5801910121S/44:user/release-keys
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model
+
+# Add Maintainer Prop
+PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Renji"
+
 
 # Release name
 PRODUCT_RELEASE_NAME := s2
